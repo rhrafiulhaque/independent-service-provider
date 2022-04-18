539b063be8aac794ba225a73cbe54b0c60c7dd72
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2022-04-18 16:58:28 +0600

    blog question and answer

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -13,8 +13,8 @@ const Blog = () => {
             <p> We are using firebase When I want to prototype product .</p>
             <p> We are implemnt password based authentication it is the most common method of authentication</p> 
             <p>We are using Multi Factor Authentication. It is the mehode that requires two or more ways to identify a user  </p>
-            <h2> Q2.  Why are you using firebase? What other options do you have to implement authentication?</h2>
-
+            <h2> Q3.  What other services does firebase provide other than authentication?</h2>
+            <p>There are many services which Firebase Provides. Most Useful them are Cloud Firestore, Cloud Function , Hosting, Google Analytics. Firebase is the full package of developing mobile or web appication faseter. Every one needs notifications in their mobile applications in one way or another for enhanced engaement with their users. Firebase is a great tool to implement notifications of the platfrom because it is fast, reliable, and scalable to handle our request.  </p>
         </div>
     );
 };
